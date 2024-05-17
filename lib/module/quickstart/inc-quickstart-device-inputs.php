7e70200199e26e76be7e56ca75d41d137e457cba
yokiowiiiz <159612037+yokiowiiiz@users.noreply.github.com>
2024-05-17 12:11:43 +0300

    Add files via upload

---
+++ lib/module/quickstart/inc-quickstart-device-inputs.php
@@ -0,0 +1,197 @@
+<div class="container-con-block darkmode-block">
+	<div class="anchor-point" id="supported_devices_devices">
+	</div>
+	<div class='container-con-block darkmode-block'>
+		<div class='container-con-wrapper'>
+			<div class='container-tx1-block darkmode-txt'>
+				<div class='container-emp-block'>
+				</div>
+				<h2>Рекомендованные оружия</h2>
+			</div>
+			<div class='container-tx2-block darkmode-txt'>
+				<p class="download-desc">
+					 Рекомендованные оружия - это те оружия с которыми будет легко в игре новичкам
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class='device-con-container'>
+		<div class='device-con-outer'>
+			<div class='device-con-inner-a'>
+				<div class='performance-con-spec performance-ico-good'>
+					<div class='performance-ico-spec' style="background: url(https://monsterhunterworld.wiki.fextralife.com/file/Monster-Hunter-World/rare-1-sword-shield-mhw_tree.png) center no-repeat; background-size:18px;">
+					</div>
+				</div>
+				<div class='device-con-gradient'>
+					<div class='device-con-graphic' style="background: url(https://cdn.kiranico.net/file/kiranico/mhworld-web/images/wp/factory/one/Nei5ik.jpg) center no-repeat; background-size:650px;">
+					</div>
+				</div>
+				<div class='device-tx1-title darkmode-txt'>
+					<span>Меч и щит</span>
+				</div>
+				<div class='device-tx2-desc darkmode-txt'>
+					<span>
+					<p>
+						 Меч и Щит — категория оружия в Monster Hunter World (MHW). Как и все оружие, оно имеет уникальный набор приемов и путь улучшения, который разветвляется в зависимости от используемых материалов. Подробную информацию об основах использования ваших охотничьих инструментов см. в разделе «Механика оружия».<br>
+						 <br>
+						 Легкий и простой в использовании меч и щит также могут похвастаться выдающейся скоростью атаки. Щит можно использовать для защиты, хотя он может не выдержать определенных атак.<br>
+						<br>
+					</p>
+					</span>
+				</div>
+			</div>
+		</div>
+		<div class='device-con-outer'>
+			<div class='device-con-inner-a'>
+				<div class='performance-con-spec performance-ico-variable'>
+					<div class='performance-ico-spec' style="background: url(https://monsterhunterworld.wiki.fextralife.com/file/Monster-Hunter-World/rare-1-greatsword-mhw_tree.png) center no-repeat; background-size:18px;">
+					</div>
+				</div>
+				<div class='device-con-gradient'>
+					<div class='device-con-graphic' style="background: url(https://cdn.kiranico.net/file/kiranico/mhworld-web/images/wp/factory/two/r8H9i4.jpg) center no-repeat; background-size:650px;">
+					</div>
+				</div>
+				<div class='device-tx1-title darkmode-txt'>
+					<span>Большой меч</span>
+				</div>
+				<div class='device-tx2-desc darkmode-txt'>
+					<span>
+					<p>
+						 Большой меч — категория оружия в Monster Hunter World (MHW). Как и все оружие, оно имеет уникальный набор приемов и путь улучшения, который разветвляется в зависимости от используемых материалов. Подробную информацию об основах использования ваших охотничьих инструментов см. в разделе «Механика оружия».<br>
+						 <br>
+						 Большой меч имеет большую дальность действия и мощные атаки. Поскольку его огромный вес ограничивает скорость вашего передвижения и атак, он хорошо подходит для тактики боя «бей и беги»<br>
+						<br>
+					</p>
+					</span>
+				</div>
+			</div>
+		</div>
+		<div class='device-con-outer'>
+			<div class='device-con-inner-a'>
+				<div class='performance-con-spec performance-ico-variable'>
+					<div class='performance-ico-spec' style="background: url(https://monsterhunterworld.wiki.fextralife.com/file/Monster-Hunter-World/rare-1-dual-blades-mhw_tree.png) center no-repeat; background-size:18px;">
+					</div>
+				</div>
+				<div class='device-con-gradient'>
+					<div class='device-con-graphic' style="background: url(https://cdn.kiranico.net/file/kiranico/mhworld-web/images/wp/factory/sou/DpF7iR.jpg) center no-repeat; background-size:650px;">
+					</div>
+				</div>
+				<div class='device-tx1-title darkmode-txt'>
+					<span>Двойные клинки</span>
+				</div>
+				<div class='device-tx2-desc darkmode-txt'>
+					<span>
+					<p>
+						 Двойные клинки — категория оружия в Monster Hunter World (MHW). Как и все оружие, оно имеет уникальный набор приемов и путь улучшения, который разветвляется в зависимости от используемых материалов. Подробную информацию об основах использования ваших охотничьих инструментов см. в разделе «Механика оружия».<br>
+						 <br>
+						 Используйте двойные клинки, чтобы обрушить на врага вихрь комбо-атак. Хотя они легкие и относительно простые в использовании, их нельзя использовать для охраны.<br>
+						<br>
+					</p>
+					</span>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class='container-con-block darkmode-block'>
+		<div class='container-con-wrapper'>
+			<div class='container-tx1-block darkmode-txt'>
+				<div class='container-emp-block'>
+				</div>
+				<h2>Supported Input Devices</h2>
+			</div>
+			<div class='container-tx2-block darkmode-txt'>
+				<p class="download-desc">
+					 RPCS3 supports a multitude of inputs ranging from Xinput, Dinput, SDL and proprietary Sony APIs.
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="gamepad-con-container">
+		<div class="gamepad-con-wrapper-gradient">
+		</div>
+		<div class="gamepad-con-wrapper">
+			<div class="gamepad-con-item">
+				<div class="gamepad-ico-logo darkmode-invert" style="background: url(/img/icons/buttons/kbm.png) center top no-repeat; background-size: cover;">
+				</div>
+				<div class="gamepad-con-tx1">
+					<span class="gamepad-con-h1">Keyboard</span>
+					<br>
+					<span class="gamepad-con-tx2">Can be used, but your experience may vary</span>
+				</div>
+				<div class="gamepad-con-icon" style="background: url(/img/graphics/quickstart/pad-keyboard.png) center no-repeat; background-size: 300px;">
+				</div>
+			</div>
+			<div class="gamepad-con-item">
+				<div class="gamepad-ico-logo darkmode-invert" style="background: url(/img/icons/buttons/xbox.png) center top no-repeat; background-size: cover;">
+				</div>
+				<div class="gamepad-con-tx1">
+					<span class="gamepad-con-h1">Xbox/Xinput</span>
+					<br>
+					<span class="gamepad-con-tx2">Works via a Bluetooth or USB connection</span>
+				</div>
+				<div class="gamepad-con-icon" style="background: url(/img/graphics/quickstart/pad-xbox.png) center no-repeat; background-size: 300px;">
+				</div>
+			</div>
+			<div class="gamepad-con-item">
+				<div class="gamepad-ico-logo darkmode-invert" style="background: url(/img/icons/buttons/playstation.png) center top no-repeat; background-size: cover;">
+				</div>
+				<div class="gamepad-con-tx1">
+					<span class="gamepad-con-h1">DualSense</span>
+					<br>
+					<span class="gamepad-con-tx2">Works via a Bluetooth or USB connection</span>
+				</div>
+				<div class="gamepad-con-icon" style="background: url(/img/graphics/quickstart/pad-dualsense.png) center no-repeat; background-size: 300px;">
+				</div>
+			</div>
+			<div class="gamepad-con-item">
+				<div class="gamepad-ico-logo darkmode-invert" style="background: url(/img/icons/buttons/playstation.png) center top no-repeat; background-size: cover;">
+				</div>
+				<div class="gamepad-con-tx1">
+					<span class="gamepad-con-h1">DualShock 4</span>
+					<br>
+					<span class="gamepad-con-tx2">Works via a Bluetooth or USB connection</span>
+				</div>
+				<div class="gamepad-con-icon" style="background: url(/img/graphics/quickstart/pad-dualshock-4.png) center no-repeat; background-size: 300px;">
+				</div>
+			</div>
+			<div class="gamepad-con-item">
+				<div class="gamepad-ico-logo darkmode-invert" style="background: url(/img/icons/buttons/playstation.png) center top no-repeat; background-size: cover;">
+				</div>
+				<div class="gamepad-con-tx1">
+					<span class="gamepad-con-h1">DualShock 3</span>
+					<br>
+					<span class="gamepad-con-tx2">Works via a Bluetooth or USB connection</span>
+				</div>
+				<div class="gamepad-con-icon" style="background: url(/img/graphics/quickstart/pad-dualshock-3.png) center no-repeat; background-size: 300px;">
+				</div>
+			</div>
+			<div class="gamepad-con-item" style="display:none">
+				<div class="gamepad-ico-logo darkmode-invert" style="background: url(/img/icons/buttons/playstation.png) center top no-repeat; background-size: cover;">
+				</div>
+				<div class="gamepad-con-tx1">
+					<span class="gamepad-con-h1">PS Move</span>
+					<br>
+					<span class="gamepad-con-tx2">Works via a Bluetooth or USB connection</span>
+				</div>
+				<div class="gamepad-con-icon" style="background: url(/img/graphics/quickstart/pad-move.png) center no-repeat; background-size: 300px;">
+				</div>
+			</div>
+			<div class="gamepad-con-item">
+				<div class="gamepad-ico-logo darkmode-invert" style="background: url(/img/icons/buttons/playstation.png) center top no-repeat; background-size: cover;">
+				</div>
+				<div class="gamepad-con-tx1">
+					<span class="gamepad-con-h1">PS Eye</span>
+					<br>
+					<span class="gamepad-con-tx2">Works via a USB connection</span>
+				</div>
+				<div class="gamepad-con-icon" style="background: url(/img/graphics/quickstart/pad-camera.png) center no-repeat; background-size: 300px;">
+				</div>
+			</div>
+			
+
+			
+			
+			
+		</div>
+	</div>
+</div>
